62abde7507f9fae7fb1503d5746daddf5bc4f7ee
DiegoEden <68476026+DiegoEden@users.noreply.github.com>
2021-10-10 18:33:20 -0600

    Arreglos dispositivos

--- www/controllers/index.js
+++ www/controllers/index.js
@@ -388,9 +388,9 @@ function getOS()
 {
     var OSName="Unknown OS";
     if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-    if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+    if (navigator.appVersion.indexOf("Mac")!=-1) OSName="iOS";
     if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-    if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+    if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Android";
     console.log(OSName);
     document.getElementById('txtOS').value=OSName;
 
